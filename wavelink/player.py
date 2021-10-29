7a6542d9895c28cf3818903a51dbaf45430ec2d2
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-10-28 21:53:50 -0700

    Update player.py

--- wavelink/player.py
+++ wavelink/player.py
@@ -173,6 +173,9 @@ class Player(discord.VoiceProtocol):
 
     async def disconnect(self, *, force: bool) -> None:
         try:
+            await self.node._websocket.send(
+                op="destroy", guildId=str(self.guild.id)
+            )
             logger.info(f"Disconnected from voice channel:: {self.channel.id}")
 
             await self.guild.change_voice_state(channel=None)
